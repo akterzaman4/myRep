599169d1f232699cf09c821aee360eae1e3a64f9
akter zaman <akterzaman4@gmail.com>
2017-02-22 22:20:50 -0500

    initial upload

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( )
+    {
+    }   
+    
+    @Test
+  public void test(){
+    System.out.println("Hello world");
+	 
+  }
+    
+    
+    @Test
+  public void test2(){
+    System.out.println("Hello to you");
+	 
+  }
+    
+}
